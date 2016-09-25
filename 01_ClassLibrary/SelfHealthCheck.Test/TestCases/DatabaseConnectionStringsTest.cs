91443648fa69301b36c67d18de6b08385e0426e0
JimInWA <jim.a.fisher.biz@gmail.com>
2016-09-24 22:01:23 -0700

    Initial code stubs

---
+++ 01_ClassLibrary/SelfHealthCheck.Test/TestCases/DatabaseConnectionStringsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SelfHealthCheck.Test.TestCases
+{
+    [TestClass]
+    public class DatabaseConnectionStringsTest
+    {
+        [TestMethod]
+        public void Driver()
+        {
+            var expectedResult = "Either no configuration file exists or no connectionString entry exists";
+
+            var databaseConnectionStrings = new DatabaseConnectionStrings();
+
+            var actualResult = databaseConnectionStrings.Validate();
+
+            Assert.AreEqual(expectedResult, actualResult, "Validate method returned unexpected result");
+        }
+    }
+}
---
+++ 01_ClassLibrary/SelfHealthCheck/DatabaseConnectionStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelfHealthCheck
+{
+    public class DatabaseConnectionStrings
+    {
+        public string Validate()
+        {
+            var connectionStringSettings = System.Configuration.ConfigurationManager.ConnectionStrings;
+
+            // if there isn't any config file, there apparently are default connection strings.
+            // if there is a config file and no connectionStrings entry, there apparently are default connection strings.
+            if (connectionStringSettings.Count < 3)
+            {
+                return "Either no configuration file exists or no connectionString entry exists";
+            }
+
+            var result = string.Empty;
+
+            for (int i = 2; i < connectionStringSettings.Count; i++)
+            {
+                var sub = connectionStringSettings[i];
+
+                result = string.Format("Name \"{0}\", ProviderName \"{1}\", ConnectionString \"{2}\"", sub.Name, sub.ProviderName, sub.ConnectionString);
+            }
+
+            return result;
+        }
+    }
+}
